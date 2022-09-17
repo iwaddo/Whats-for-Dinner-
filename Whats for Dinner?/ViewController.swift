2e935af324f3d8de4e19fb58b64eb4e8bd6c5bb5
Ian Waddington <iwaddo@icloud.com>
2022-09-17 11:35:49 +0100

    Ch. 2.9 Loading New Images

--- Whats for Dinner?/ViewController.swift
+++ Whats for Dinner?/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dinnerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        dinnerLabel.text = ""
     }
 
-
+    
+    
+    @IBAction func dinnerButton(_ sender: UIButton) {
+        let dinnerNumber = Int.random(in: 1...4)
+        imageView.image = UIImage(named: "dinner\(String(dinnerNumber))")
+        if dinnerNumber == 1 {
+            dinnerLabel.text = "Pizza"
+        } else if dinnerNumber == 2 {
+            dinnerLabel.text = "Burritos"
+        } else if dinnerNumber == 3 {
+            dinnerLabel.text = "Susshi"
+        } else if dinnerNumber == 4 {
+            dinnerLabel.text = "Cobb Salad"
+        } else {
+            dinnerLabel.text = "Error"
+        }
+        
+    }
+    
 }
 
